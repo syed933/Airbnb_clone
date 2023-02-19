278c75b504a0282753b7c0e02b9ae9d51ff8a7e6
Syed <syehassan@gmail.com>
2023-02-18 20:26:48 -0800

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Banner from 'components/Banner'
 import SmallCard from 'components/SmallCard';
 import MediumCard from 'components/MediumCard';
 import LargeCard from 'components/LargeCard';
+import Footer from 'components/Footer';
 
 
 export default function Home({ exploreData, cardsData }) {
@@ -69,8 +70,8 @@ export default function Home({ exploreData, cardsData }) {
             description='Wishlists curated by Airbnb.'
             buttonText='Get Inspired'  
           />
-
         </main>
+        <Footer />
       </div>
     </>
   );
